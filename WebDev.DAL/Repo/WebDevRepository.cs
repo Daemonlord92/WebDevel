6deef4624991de16a2f37fd09e0cc36fc74a92a6
Matthew Martin <matthew-martin@programmer.net>
2022-07-05 03:16:43 -0400

    Setup the Repository file with constructor in place.

---
+++ WebDev.DAL/Repo/WebDevRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDev.DAL.Models;
+
+namespace WebDev.DAL.Repo
+{
+    public class WebDevRepository
+    {
+        WebDevPortDBCoreContext context;
+
+        public WebDevRepository()
+        {
+            context = new WebDevPortDBCoreContext();
+        }
+
+    }
+}
