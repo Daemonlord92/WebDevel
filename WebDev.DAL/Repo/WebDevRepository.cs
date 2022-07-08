dd1cbf951b96dc5e3911eb7acfae8590aeebadc8
Matthew Martin <matthew-martin@programmer.net>
2022-07-08 13:45:47 -0400

    Created the RegisterUser Repo Function

--- WebDev.DAL/Repo/WebDevRepository.cs
+++ WebDev.DAL/Repo/WebDevRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,5 +18,23 @@ namespace WebDev.DAL.Repo
             context = new WebDevPortDBCoreContext();
         }
 
+        public int RegisterUser(string username, string password, string emailId)
+        {
+            int result;
+            SqlParameter prmUsername = new SqlParameter("@Username", username);
+            SqlParameter prmPassword = new SqlParameter("@Password", password);
+            SqlParameter prmEmailId = new SqlParameter("@EmailId", emailId);
+
+            try
+            {
+                result = context.Database.ExecuteSqlRaw("EXEC usp_RegisterUser @Username, @Password, @EmailId", new[] { prmUsername, prmPassword, prmEmailId });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                result = -99;
+            }
+            return result;
+        }
     }
 }
---
+++ WebDevTest/Program.cs
@@ -0,0 +1,9 @@
+﻿using WebDev.DAL.Repo;
+
+WebDevRepository repository = new WebDevRepository();
+#region CreateUser
+int status;
+//status = repository.RegisterUser("Smokeintitan", "123456789", "Testuser@test.com");
+//Console.WriteLine(status);
+#endregion
+
