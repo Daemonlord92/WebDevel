be3eb3701d26176336e5326d40a462e257521c09
Matthew Martin <matthew-martin@programmer.net>
2022-07-10 08:52:31 -0400

    Created files to start the BugTracker Routes.

---
+++ WebDev.API/Models/Bug.cs
@@ -0,0 +1,11 @@
+﻿namespace WebDev.API.Models
+{
+    public class Bug
+    {
+        public int BugId { get; set; }
+        public string? BugName { get; set; }
+        public string? BugDescription { get; set; }
+        public string? GitUrl { get; set; }
+        public int UserId { get; set; }
+    }
+}
