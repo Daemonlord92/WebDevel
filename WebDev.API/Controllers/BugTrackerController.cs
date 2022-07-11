a34d75dc9282e133a8597cb76c7de9d76d644f77
Matthew Martin <matthew-martin@programmer.net>
2022-07-11 10:11:10 -0400

    Created the the postNewBug. Updated the README to match.

--- WebDev.API/Controllers/BugTrackerController.cs
+++ WebDev.API/Controllers/BugTrackerController.cs
@@ -1,11 +1,36 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebDev.API.Models;
+using WebDev.DAL;
 
 namespace WebDev.API.Controllers
 {
-    [Route("api/[controller]/[action]")]
+    [Route("APIv1/[controller]/[action]")]
     [ApiController]
     public class BugTrackerController : ControllerBase
     {
+
+        DAL.Repo.WebDevRepository repository;
+
+        public BugTrackerController()
+        {
+            repository = new DAL.Repo.WebDevRepository();
+        }
+
+        [HttpPost]
+        public int PostNewBug(Bug bug)
+        {
+            int result;
+            try
+            {
+                result = repository.PostNewBug(bug.BugName, bug.BugDescription, bug.GitUrl, bug.UserId);
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                result = -99;
+            }
+            return result;
+        }
     }
 }
