6f74e482148d23051481b884303688d84a8dc54d
Matthew Martin <matthew-martin@programmer.net>
2022-07-12 10:58:58 -0400

    Minor updates, and linked the Contact-Me page to the Navbar.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactMeComponent } from './component/static/contact-me/contact-me.component';
 import { HomeComponent } from './component/static/home/home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'contactme', component: ContactMeComponent },
   { path:'home', component: HomeComponent }
 ];
 
